8459ea8bc6425a2572d390c37d0297dd72f8b6c4
recepayddogdu <recepayddogdu@gmail.com>
2020-04-05 02:20:12 +0300

    Fonksiyon nasıl yazılır?

---
+++ Python/Python Tutorial/Functions.py
@@ -0,0 +1,97 @@
+# Matematiksel islemler
+
+4*4
+4/4
+4-2
+4+2 # bunlar klasik matematiksel operatorlerdir.
+
+3**2 # 3'un 2'nci kuvveti
+
+3**3 # 3'un 3'ncu kuvveti
+
+# =============================================================================
+# #Fonksiyon Nasil Yazilir?
+
+def kare_al(x):
+    print(x**2) # def ile fonksiyon olusturacagimizi belirtiriz.
+
+kare_al(5) #fonksiyonu bu sekilde calistiririz.
+
+# =============================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
