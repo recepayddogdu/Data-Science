603ccde7734b699ddbb701d4104ce712fa76d2cd
recepayddogdu <recepayddogdu@gmail.com>
2020-07-21 00:49:33 +0300

    Create select.sql

---
+++ SQL/Temel_SQL_Komutlari/select.sql
@@ -0,0 +1,9 @@
+SELECT ID, CUSTOMERNAME, CITY -- Column isimleri
+FROM CUSTOMER
+
+SELECT * FROM CUSTOMER -- '*' Sembolu ile tum column'lari birden cekebiliriz. 
+
+--SELECT [KOLON ÝSMÝ] FROM CUSTOMER
+/*Eger kolon ismi bosluk, turkce karakter gibi seyler iceren bir
+  yapida ise koseli parantez icerisine yazmamiz gerekir. */
+
