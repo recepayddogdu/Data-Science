9146be017fc71b3222994bd4e17128aafa940688
recepayddogdu <recepayddogdu@gmail.com>
2020-07-20 20:36:46 +0300

    SQL

--- SQL/SQL_Baslangic/Temel SQL Komutları/Top.sql
+++
@@ -1,6 +0,0 @@
-select top 5 * from CUSTOMER --ilk 5 sonucu listeler
-where CITY='istanbul'
-order by CUSTOMERNAME
----------------------------
-select top 50 percent*from CUSTOMER --yuzde 50'sini listeler
-order by CUSTOMERNAME
--- SQL/SQL_Baslangic/Temel SQL Komutları/selectinto.sql
+++
@@ -1,1 +0,0 @@
-select*into customeryedek from CUSTOMER --customeryedek adinda yeni table olustur ve CUSTOMER icindekileri oraya kopyalas
