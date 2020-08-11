6b83e8bddba9aa5417e7cddde788bcb590e1d425
recepayddogdu <recepayddogdu@gmail.com>
2020-08-12 01:31:36 +0300

    SQL_Path_change

--- SQL/Temel_SQL_Komutlari/update.sql
+++
@@ -1,8 +0,0 @@
-
---Yas hesaplayip AGE kolonuna yazdirmak istiyoruz.
---SQL'in kendi fonksiyonlarindan olan DATEDIFF fonksiyonunu kullanacagiz.
-
-UPDATE CUSTOMER 
-SET AGE=DATEDIFF(YEAR,BIRTHDATE,GETDATE())
-
-SELECT * FROM CUSTOMER
